0d65466248db6d2b208467f4e8d0827555fca15e
Carlo <carloalessi94@gmail.com>
2019-07-05 23:30:54 +0200

    quasi finito

--- Assignment4/Main_Luca.cpp
+++ Assignment4/Main_Luca.cpp
@@ -2,123 +2,33 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <type_traits>
+#include <typeinfo>
 #include "./map_reduce_Luca.h"
-
-
-//SafeQueue<std::pair<int, int>> global_results;
-
-int findItem(std::vector<std::pair<int, int>> local_results, int item) {
-    for (int i = 0; i < local_results.size(); i++) {
-        if (local_results[i].first == item) {
-            return i;
-        }
-    }
-    return -1;
-}
-
-void increment_or_insert_item(std::vector<std::pair<int, int>> local_results, int item) {
-    int index = findItem(local_results, item);
-    if (index == -1) {
-        local_results.push_back(std::make_pair(item, 1));
-    } else {
-        local_results[index].second++;
-    }
-}
 
 template <typename T>
 std::pair<T, int> count_numbers_map(T item) {
     return std::make_pair(item, 1);
 }
 
-//std::pair<int, int> count_numbers_map(int item, std::vector<SafeQueue<std::pair<int, int>>*> results) {
-
-//std::vector<std::pair<int, int>> local_results;
-//for (int i = startIdx; i <= endIdx; i++) {
-//int item = data[i];
-//increment_or_insert_item(local_results, item);
-//int index = findItem(results, item);
-//return std::make_pair(item, 1);
-//if (index == -1) {
-//  results.push_back(std::make_pair(item, 1));
-//  } else {
-//    results[index].second++;
-//}
-//}
-
-// push local results to reducers queue
-//int nw_reducers = results.size();
-//for (auto i : local_results)
-//    results[i.first % nw_reducers]->safe_push(i);
-//}
-
-std::vector<std::pair<int, int>> count_numbers_reduce(SafeQueue<std::pair<int, int>>* queue) {
-    std::vector<std::pair<int, int>> local_results;
-
-    while (true) {
-        auto pair = queue->safe_pop();
-        if (pair.first == -1 && pair.second == -1)
-            break;
-
-        //increment_or_insert_item(local_results, pair.first);
-        int index = findItem(local_results, pair.first);
-        if (index == -1) {
-            local_results.push_back(std::make_pair(pair.first, 1));
-        } else {
-            local_results[index].second++;
-        }
-    }
-
-    return local_results;
-}
-
 int main(int argc, char* argv[]) {
     int nw_mapper = atoi(argv[1]);
     int nw_reducer = atoi(argv[2]);
+    //int type_data = atoi(argv[3]);
 
-    std::vector<int> data = {2, 3, 3, 4, 6, 2, 4, 6, 6, 5, 6, 1, 1, 1, 1, 1};
+
+    std::cout << std::is_same<typeid("ciao").name(), typeid("").name()>::value << std::endl;
+
+    //std::vector<int> data = {2, 3, 3, 4, 6, 2, 4, 6, 6, 5, 6, 1, 1, 1, 1, 1};
+    std::vector<std::string> data = {"ciao", "mondo", "roma", "lazio", "italia", "lex", "ciao", "italia", "roma"};
+
     for (auto i : data)
         std::cout << i << " ";
     std::cout << std::endl;
 
-    MapReduce<int> mr(nw_mapper, nw_reducer);
-    mr.map_and_reduce(data, count_numbers_map<int>);
+    MapReduce<std::string> mr(nw_mapper, nw_reducer);
+    mr.map_and_reduce(data, count_numbers_map<std::string>);
     mr.print_results();
-
-    //std::vector<std::thread> mappers(nw_mapper);
-    //std::vector<std::thread> reducers(nw_reducer);
-
-    //std::vector<SafeQueue<std::pair<int, int>>*> results(nw_reducer);
-    //for(int i = 0; i < nw_reducer; i++)
-    //  results[i] = new SafeQueue<std::pair<int, int>>;
-
-    //int dim = data.size() / nw_mapper;
-    // start mappers
-    //for(int i = 0; i < nw_mapper; i++){
-    //int startIdx = i * dim;
-    //int endIdx = i == nw_mapper-1 ? data.size()-1 : (i+1) * dim-1;
-    //  mappers[i] = std::thread(count_numbers_map, data, startIdx,  endIdx, results);
-    //}
-
-    //start reducers
-    //for(int i = 0; i < nw_reducer; i++)
-    //  reducers[i] = std::thread(count_numbers_reduce, results[i]);
-
-    // send termination signal to reducers
-    //for(int i = 0; i < nw_reducer; i++)
-    //  results[i]->safe_push(std::make_pair(-1,-1));
-
-    // join mappers and reducers
-    //for(int i=0;i<nw_mapper;i++)
-    //  mappers[i].join();
-    //for(int i=0;i<nw_reducer;i++)
-    //  reducers[i].join();
-
-    //std::vector<std::pair<int, int>> data_mapped = count_numbers_map(data, 0, data.size()-1);
-
-    //while(!global_results.isEmpty()){
-    //  auto item = global_results.safe_pop();
-    //  std::cout << item.first << "," << item.second << std::endl;
-    //}
 
     return 0;
 }
--- Assignment4/map_reduce_Luca.h
+++ Assignment4/map_reduce_Luca.h
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <typeinfo>
 #include "./safe_queue.h"
 
 
@@ -11,10 +12,11 @@ class MapReduce {
    private:
     int nw_mapper;                                              // number of threads for the map
     int nw_reducer;                                             // number of threads for the reducer
-    SafeQueue<std::pair<int, int>> global_results;              // final results - combination of local results
+    SafeQueue<std::pair<T, int>> global_results;              // final results - combination of local results
     std::vector<SafeQueue<std::pair<T, int>> *> local_results;  // results of each reducer
     std::vector<std::thread> mappers;
     std::vector<std::thread> reducers;
+    std::hash<T> hasher;
 
    public:
     MapReduce(int nw_mapper, int nw_reducer) : nw_mapper(nw_mapper),
@@ -23,10 +25,10 @@ class MapReduce {
                                                reducers(nw_reducer),
                                                local_results(nw_reducer) {
         for (int i = 0; i < nw_reducer; i++)
-            local_results[i] = new SafeQueue<std::pair<int, int>>;
+            local_results[i] = new SafeQueue<std::pair<T, int>>;
     };
 
-    int findItem(std::vector<std::pair<T, int>> local_results, int item) {
+    int findItem(std::vector<std::pair<T, int>> local_results, T item) {
         for (int i = 0; i < local_results.size(); i++)
             if (local_results[i].first == item)
                 return i;
@@ -34,9 +36,8 @@ class MapReduce {
     }
 
     //private:
-    //std::pair<T,int>
     void map(std::function<std::pair<T, int>(T)> f_map,
-             std::vector<int> data, int startIdx, int endIdx) {
+             std::vector<T> data, int startIdx, int endIdx) {
         std::vector<std::pair<T, int>> temp_results;
 
         for (int i = startIdx; i <= endIdx; i++) {
@@ -54,15 +55,16 @@ class MapReduce {
         //push local results to reducers queue
         int nw_reducers = local_results.size();
         for (auto i : temp_results)
-            local_results[i.first % nw_reducers]->safe_push(i);
+            local_results[hasher(i.first) % nw_reducers]->safe_push(i);
     }
 
-    void reduce(SafeQueue<std::pair<int, int>> *queue) {
+    void reduce(SafeQueue<std::pair<T, int>> *queue) {
         std::vector<std::pair<T, int>> temp_results;
 
         while (true) {
             auto pair = queue->safe_pop();
-            if (pair.first == -1 && pair.second == -1)
+            //if (pair.first == -1 && pair.second == -1)
+            if (pair.first == "" && pair.second == -1)
                 break;
 
             int index = this->findItem(temp_results, pair.first);
@@ -74,13 +76,12 @@ class MapReduce {
 
         // push to global results
         for (auto i : temp_results) {
-            //std::cout << local_results.size() << " " << i.first << " " << i.second << std::endl;
             global_results.safe_push(i);
         }
     }
 
-    void map_and_reduce(std::vector<int> data,
-                        std::function<std::pair<T, int>(int)> f_map) {
+    void map_and_reduce(std::vector<T> data,
+                        std::function<std::pair<T, int>(T)> f_map) {
         int dim = data.size() / nw_mapper;
 
         // start mappers
@@ -100,7 +101,8 @@ class MapReduce {
 
         // send termination signal to reducers
         for (int i = 0; i < nw_reducer; i++)
-            local_results[i]->safe_push(std::make_pair(-1, -1));
+            //local_results[i]->safe_push(std::make_pair(-1, -1));
+            local_results[i]->safe_push(std::make_pair<T,int>("", -1));
 
         // join reducers
         for (int i = 0; i < nw_reducer; i++)
@@ -108,13 +110,6 @@ class MapReduce {
     }
 
     void print_results() {
-        // std::cout << nw_mapper << std::endl;
-        // std::cout << nw_reducer << std::endl;
-        // std::cout << global_results.safe_size() << std::endl;
-        // std::cout << local_results.size() << std::endl;
-        // std::cout << mappers.size() << std::endl;
-        // std::cout << reducers.size() << std::endl;
-
         while (!global_results.isEmpty()) {
             auto item = global_results.safe_pop();
             std::cout << item.first << "," << item.second << std::endl;
