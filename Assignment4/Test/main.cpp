c1cebe713fe13390ac3abbc7509d8e8392a7c081
Luca <2198490+lucacorbucci@users.noreply.github.com>
2019-07-05 23:41:08 +0200

    Create main.cpp

---
+++ Assignment4/Test/main.cpp
@@ -0,0 +1,29 @@
+#include <unistd.h>
+#include <iostream>
+#include <type_traits>
+#include <typeinfo>
+#include <utility>
+#include <vector>
+#include "./MapReduce.h"
+
+template <typename T>
+std::pair<T, int> count_numbers_map(T item) {
+    return std::make_pair(item, 1);
+}
+
+int main(int argc, char* argv[]) {
+    int nw_mapper = atoi(argv[1]);
+    int nw_reducer = atoi(argv[2]);
+
+    std::vector<std::string> data = {"ciao", "mondo", "roma", "lazio", "italia", "lex", "ciao", "italia", "roma"};
+
+    for (auto i : data)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    MapReduce<std::string> mr(nw_mapper, nw_reducer);
+    mr.map_and_reduce(data, count_numbers_map<std::string>);
+    mr.print_results();
+
+    return 0;
+}
